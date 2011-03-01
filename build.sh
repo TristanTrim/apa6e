ba960e4a63475f5eebf2291bc2ad9912221c0024
Nathaniel Smith <njs@pobox.com>
2011-03-01 14:44:36 -0800

    put the files for distribution inside a subdirectory

--- build.sh
+++ build.sh
@@ -15,9 +15,13 @@ $LATEX ${CLASS}.dtx
 rm -f ${CLASS}.cls
 $LATEX ${CLASS}.ins
 
-# CTAN uploads generally don't have version numbers on them:
+# CTAN uploads generally don't have version numbers on them, and are flat:
 rm -f ${CLASS}.zip
 zip ${CLASS}.zip ${DISTFILES}
-# But uploads anywhere else do:
-zip ${CLASS}-${VERSION}.zip ${DISTFILES}
+# But uploads anywhere else do, and are nested:
+rm -rf ${CLASS}-${VERSION}
+mkdir ${CLASS}-${VERSION}/
+cp ${DISTFILES} ${CLASS}-${VERSION}/
+zip -r ${CLASS}-${VERSION}.zip ${CLASS}-${VERSION}/
+rm -rf ${CLASS}-${VERSION}
 echo "Auto-detected version is: ${VERSION}"
